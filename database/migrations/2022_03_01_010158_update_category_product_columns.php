2b926e9782f752fb03053f330f7db9d7790f7922
Nigel Kislew William <65957150+NigelTheFrog@users.noreply.github.com>
2022-03-01 08:11:04 +0700

    1

--- database/migrations/2022_03_01_010158_update_category_product_columns.php
+++ database/migrations/2022_03_01_010158_update_category_product_columns.php
@@ -16,16 +16,6 @@ class UpdateCategoryProductColumns extends Migration
         Schema::table('categories', function (Blueprint $table) {
             $table->string('description',5000);
         });
-
-        Schema::table('products', function (Blueprint $table) {
-            $table->string('form');
-            $table->string('formula')->nullable();
-            $table->string('description')->nullable();
-            $table->boolean('faskes_1')->default(false);
-            $table->boolean('faskes_2')->default(false);
-            $table->boolean('faskes_3')->default(false);
-            $table->timestamps();
-        });
     }
 
     /**
@@ -35,9 +25,6 @@ class UpdateCategoryProductColumns extends Migration
      */
     public function down()
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn('form', 'formula', 'description', 'faskes_1', 'faskes_2', 'faskes_3');
-        });
         Schema::table('categories', function (Blueprint $table) {
             $table->dropColumn('description');
         });
